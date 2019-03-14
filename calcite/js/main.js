4a76be29b17b7da68a6b7a79e4259559df954b19
Carlos Guerrero López <c.guerrerolopez@gmail.com>
2019-03-14 23:49:28 +0100

    add widgets, draw, print, etc

--- calcite/js/main.js
+++ calcite/js/main.js
@@ -10,6 +10,16 @@ require([
     "esri/dijit/Directions",
     "dojo/parser",
     "esri/urlUtils",
+    "esri/layers/FeatureLayer",
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+    "esri/dijit/Legend",
+    "esri/dijit/Print",
+    "esri/toolbars/draw",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/symbols/SimpleLineSymbol",
+    "esri/Color",
+    "esri/graphic", 
+    "esri/layers/GraphicsLayer",
     // Bootstrap
     "bootstrap/Collapse", 
     "bootstrap/Dropdown",
@@ -17,7 +27,8 @@ require([
 
     "dojo/domReady!"
 ], function(Map, Search,  CalciteMaps,
-    Directions,parser,urlUtils,esriId) {
+    Directions,parser,
+    urlUtils,FeatureLayer,ArcGISDynamicMapServiceLayer,Legend,Print,Draw,SimpleMarkerSymbol,SimpleLineSymbol,Color,Graphic,GraphicsLayer) {
         
     parser.parse();
     //all requests to route.arcgis.com will proxy to the proxyUrl defined in this object.
@@ -31,31 +42,73 @@ require([
     });
     // App 
     app = {
-    map: null,
-    basemap: "dark-gray",
-    center: [-40, 40], // lon, lat
-    zoom: 3,
-    initialExtent: null,
-    searchWidgetNav: null,
-    searchWidgetPanel: null
+        map: null,
+        basemap: "dark-gray",
+        center: [-40, 40], // lon, lat
+        zoom: 3,
+        initialExtent: null,
+        searchWidgetNav: null,
+        searchWidgetPanel: null,
+        urlMapaDinamico:"https://sampleserver6.arcgisonline.com/arcgis/rest/services/Wildfire/MapServer",
+        urlFeatureLayer:"https://sampleserver6.arcgisonline.com/arcgis/rest/services/Earthquakes_Since1970/MapServer/0",
+        urlPrint:"https://sampleserver5.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task",
+        idCapaGrafica:"micapa"
+        
     }
 
     // Map 
     app.map = new Map("mapViewDiv", {
-    basemap: app.basemap,
-    center: app.center,
-    zoom: app.zoom
+        basemap: app.basemap,
+        center: app.center,
+        zoom: app.zoom
     });
 
     app.map.on("load", function(){
         app.initialExtent = app.map.extent;
+        app.tb = new Draw(app.map);
+        app.tb.on("draw-end", addGraphic);
+        app.map.addLayer(new GraphicsLayer({
+            id:app.idCapaGrafica
+        }));
     });
 
-    // Search
-    app.searchDivNav = createSearchWidget("searchNavDiv");
-    app.searchWidgetPanel = createSearchWidget("searchPanelDiv");
+    _enlazarEventos();
 
-    function createSearchWidget(parentId) {
+    _creaciondeWidgets();
+
+    
+    
+    function _creaciondeWidgets(){
+        // Search
+        app.searchDivNav = _createSearchWidget("searchNavDiv");
+        app.searchWidgetPanel = _createSearchWidget("searchPanelDiv");
+
+        //Direcciones
+        var directions = new Directions({
+            map:  app.map
+          },"dir");
+          //directions.startup();
+        //Leyenda
+        var legend = new Legend({
+            map: app.map,
+            }, "leyenda");
+        legend.startup();
+        //Impresion
+        app.printer = new Print({
+            map: app.map,
+            url: app.urlPrint
+          }, document.getElementById("btnImprimir"));
+        app.printer.startup();
+    }
+   
+    function addGraphic(evt) {
+        app.tb.deactivate(); 
+        app.map.enableMapNavigation();
+        let capa= app.map.getLayer(app.idCapaGrafica);
+        capa.add(new Graphic(evt.geometry,app.markerSymbol));
+    }
+
+    function _createSearchWidget(parentId) {
         var search = new Search({
             map: app.map,
             enableHighlight: false
@@ -63,14 +116,64 @@ require([
         search.startup();
         return search;
     }
+
+    function _enlazarEventos(){
+        try{
+            document.getElementById("btnCargarFeatureLayer").onclick =_cargarFeatureLayer;
+            document.getElementById("btnCargarMapa").onclick =_cargarMapa;
+            document.getElementById("btnDibujarPunto").onclick =_dibujarPunto;
+            
+            
+        }catch(ex){
+            console.error("Error al enlazar eventos")
+        }
+    }
+    /**Eventos*/
+    //Carga una capa de tipo featureLayer
+    function _cargarFeatureLayer(){
+        let nombreCapa="featureLayer";
+        if(app.map.graphicsLayerIds.indexOf(nombreCapa)>-1){
+            app.map.getLayer(nombreCapa).setVisibility(!app.map.getLayer(nombreCapa).visible);
+        }else{
+            let featureLayer= new FeatureLayer(app.urlFeatureLayer, {
+                showAttribution :false,
+                id:nombreCapa
+            });
+            
+            app.map.addLayer(featureLayer);
+        }
+    }
+    //Carga una capa de tipo ArcGISDynamicMapServiceLayer
+    function _cargarMapa(){
+        let nombreCapa="dinamic";
+        if(app.map.layerIds.indexOf(nombreCapa)>-1){
+            app.map.getLayer(nombreCapa).setVisibility(!app.map.getLayer(nombreCapa).visible);
+        }else{
+            let  dinamic = new ArcGISDynamicMapServiceLayer(app.urlMapaDinamico, {
+                "showAttribution": false,
+                id:nombreCapa
+              });
+            app.map.addLayer(dinamic);
+        }
+    }
     // Basemaps
     document.getElementById("selectBasemapPanel").onchange=function(e){
         app.map.setBasemap(e.target.options[e.target.selectedIndex].value);
     };
+    //Dibujo
+    function _dibujarPunto(){
+        app.map.disableMapNavigation();
+        app.tb.activate("point");
+    }
 
-    var directions = new Directions({
-        map:  app.map
-      },"dir");
-      //directions.startup();
+    (function _crearSimbologias(){
+        app.markerSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 20,
+            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+            new Color([255,0,0]), 1),
+            new Color([255,0,255,0.5]));
+    })();
+
+
+    
 
 });
